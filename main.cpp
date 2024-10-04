b1912dcf6579fc1f4ee9d1dfa575b7d0a6e2ed3f
Vevaan <vevaan.s.verma@gmail.com>
2024-10-03 23:00:35 -0700

    Add node insertion methods

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
+// COMSC-210 | Lab 17 | Vevaan Verma
 #include <iostream>
 using namespace std;
 
+struct Node;
+
+/* CONSTANTS */
 const int SIZE = 7;
 
-struct Node {
-	float value;
-	Node* next;
-};
-
+/* FUNCTION PROTOTYPES */
+void createList(Node*&);
+void addNodeFront(Node*&, float);
+void addNodeBack(Node*&, float);
+void insertNode(Node*&, float, int);
+void deleteNode(Node*&, int);
+void deleteList(Node*&);
 void output(Node*);
 
+struct Node {
+
+	float value;
+	Node* next;
+
+};
+
+// main() is the entry point of the program
+// arguments: none
+// returns: int - the exit code of the program
 int main() {
 
 	Node* head = nullptr;
@@ -130,6 +146,97 @@ int main() {
 	output(head);
 
 	return 0;
+
+}
+
+// createList() creates a linked list of size SIZE with random numbers 0-99
+// arguments: Node*& - the head of the linked list
+// returns: none
+void createList(Node*& head) {
+
+	// create a linked list of size SIZE with random numbers 0-99
+	for (int i = 0; i < SIZE; i++)
+		addNodeFront(head, rand() % 100); // add the new node to the front of the list
+
+}
+
+// addNodeFront() adds a node to the front of the linked list
+// arguments: Node*& - the head of the linked list, float - the value of the new node
+// returns: none
+void addNodeFront(Node*& head, float nodeVal) {
+
+	Node* newNode = new Node;
+	newNode->value = nodeVal;
+	newNode->next = head;
+	head = newNode;
+
+}
+
+// addNodeBack() adds a node to the back of the linked list
+// arguments: Node*& - the head of the linked list, float - the value of the new node
+// returns: none
+void addNodeBack(Node*& head, float nodeVal) {
+
+	Node* newNode = new Node;
+	newNode->value = nodeVal;
+	newNode->next = nullptr;
+
+	// if the list is empty, the new node is the head
+	if (!head) {
+
+		head = newNode;
+
+	} else {
+
+		Node* current = head;
+
+		// traverse the list to the last node
+		while (current->next)
+			current = current->next;
+
+		// add the new node to the end of the list
+		current->next = newNode;
+
+	}
+}
+
+// insertNode() inserts a node into the linked list after a specified index
+// arguments: Node*& - the head of the linked list, int - the index of the node to insert after
+// returns: none
+void insertNode(Node*& head, float nodeVal, int index) {
+
+	// make sure the list isn't empty
+	if (!head) {
+
+		cout << "Empty list.\n";
+		return;
+
+	}
+
+	Node* current = head;
+	int count = 1;
+
+	// traverse the list to the node to insert after
+	while (current && count < index) {
+
+		current = current->next;
+		count++;
+
+	}
+
+	// if the index is out of bounds, output an error message
+	if (!current) {
+
+		cout << "Index out of bounds.\n";
+		return;
+
+	}
+
+	// create the new node
+	Node* newNode = new Node;
+	newNode->value = nodeVal;
+	newNode->next = current->next; // the new node points to the node after the current node
+	current->next = newNode; // the current node points to the new node
 
 }
 
